6ba56e6369b7ed9c6cc578c7164a53ebc861357c
Shahrom <mshahromali@gmail.com>
2023-05-09 18:14:57 -0400

    Adding fonts

--- App.js
+++ App.js
@@ -1,5 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+
+import {
+  useFonts,
+  Urbanist_100Thin,
+  Urbanist_200ExtraLight,
+  Urbanist_300Light,
+  Urbanist_400Regular,
+  Urbanist_500Medium,
+  Urbanist_600SemiBold,
+  Urbanist_700Bold,
+  Urbanist_800ExtraBold,
+  Urbanist_900Black,
+  Urbanist_100Thin_Italic,
+  Urbanist_200ExtraLight_Italic,
+  Urbanist_300Light_Italic,
+  Urbanist_400Regular_Italic,
+  Urbanist_500Medium_Italic,
+  Urbanist_600SemiBold_Italic,
+  Urbanist_700Bold_Italic,
+  Urbanist_800ExtraBold_Italic,
+  Urbanist_900Black_Italic,
+} from "@expo-google-fonts/urbanist";
+
+
+import WelcomeScreen from "./src/Views/StartScreen";
 
 export default function App() {
   return (
@@ -9,6 +35,30 @@ export default function App() {
     </View>
   );
 }
+  let [fontsLoaded] = useFonts({
+    Urbanist_100Thin,
+    Urbanist_200ExtraLight,
+    Urbanist_300Light,
+    Urbanist_400Regular,
+    Urbanist_500Medium,
+    Urbanist_600SemiBold,
+    Urbanist_700Bold,
+    Urbanist_800ExtraBold,
+    Urbanist_900Black,
+    Urbanist_100Thin_Italic,
+    Urbanist_200ExtraLight_Italic,
+    Urbanist_300Light_Italic,
+    Urbanist_400Regular_Italic,
+    Urbanist_500Medium_Italic,
+    Urbanist_600SemiBold_Italic,
+    Urbanist_700Bold_Italic,
+    Urbanist_800ExtraBold_Italic,
+    Urbanist_900Black_Italic,
+  });
+  if (!fontsLoaded) {
+    // Refactor: Replace with splash screen
+    return <AppLoading />;
+  } 
 
 const styles = StyleSheet.create({
   container: {
---
+++ src/Utils/Fonts.js
@@ -0,0 +1,34 @@
+const Fonts = {
+  // All Fonts: 
+    Black: "Urbanist_900Black",
+    ExtraBold: "Urbanist_800ExtraBold",
+    Bold: "Urbanist_700Bold",
+    SemiBold: "Urbanist_600SemiBold",
+    Medium: "Urbanist_500Medium",
+    Regular: "Urbanist_400Regular",
+    Light: "Urbanist_300Light",
+    ExtraLight: "Urbanist_200ExtraLight",
+    Thin:  "Urbanist_100Thin",
+
+    BlackItalic: "Urbanist_900Black_Italic",
+    ExtraBoldItalic: "Urbanist_800ExtraBold_Italic",
+    BoldItalic: "Urbanist_700Bold_Italic",
+    SemiBoldItalic: "Urbanist_600SemiBold_Italic",
+    MediumItalic: "Urbanist_500Medium_Italic",
+    RegularItalic: "Urbanist_400Regular_Italic",
+    LightItalic: "Urbanist_300Light_Italic",
+    ExtraLightItalic: "Urbanist_200ExtraLight_Italic",
+    ThinItalic:  "Urbanist_100Thin_Italic",
+
+    //Font Hierarchy
+    Title: "Urbanist_800ExtraBold",
+    Heading: "Urbanist_700Bold",
+    Subheading: "Urbanist_600SemiBold",
+    BodyText: "Urbanist_400Regular",
+    ButtonTextLight: "Urbanist_500Medium", 
+    ButtonText: "Urbanist_700Bold", 
+    ButtonTextExtraBold: "Urbanist_800ExtraBold", 
+  };
+  
+  export default Fonts;
+  
