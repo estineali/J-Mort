b4a9936bd52ab6497a21528b1f342580bd0d77d4
Shahrom <mshahromali@gmail.com>
2023-05-31 16:56:17 -0400

    Update ColorPalette.js

--- src/Utils/ColorPalette.js
+++ src/Utils/ColorPalette.js
@@ -1,8 +1,16 @@
 const ColorPalette = {
-    "white" : "#FFFFFF",
-    "black" : "#000000",
-    "DarkGrey" : "#030303",
-    "Orange" : "#FFA000",
-}
+  white: "#FFFFFF",
+  black: "#000000",
+  lightGrey: "#F4F4F4",
+  DarkGrey: "#030303",
+  Orange: "#FFA000",
+  Blue: "#A210FF",
+
+  backgroundColor: "#f3f3f3",
+
+  chatBubble: "#F4F4F4",
+  chatBubbleAI: "#8696BC",
+  chatBubbleUser: "#F7F399",
+};
 
 export default ColorPalette;
