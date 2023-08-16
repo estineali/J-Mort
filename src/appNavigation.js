8f3466cdba6c7be14150a6bf64f2066b31029140
Shahrom <mshahromali@gmail.com>
2023-08-16 06:21:56 -0400

    disabling screen headers

--- src/appNavigation.js
+++ src/appNavigation.js
@@ -3,12 +3,14 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SplashScreen from "./Views/StartScreen";
 import ChatScreen from "./Views/chatScreen";
 
-
 const NavStack = createNativeStackNavigator();
 
 export default Screens = () => {
   return (
-    <NavStack.Navigator initialRouteName="splash-screen">
+    <NavStack.Navigator
+      initialRouteName="splash-screen"
+      screenOptions={{ headerShown: false }}
+    >
       <NavStack.Screen name="splash-screen" component={SplashScreen} />
       <NavStack.Screen name="chat-screen" component={ChatScreen} />
     </NavStack.Navigator>
