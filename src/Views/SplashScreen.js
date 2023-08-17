8337e8d302a3cffa708e793712bac870884c6778
Shahrom <mshahromali@gmail.com>
2023-08-17 14:02:27 -0400

    username and password inputs

--- src/Views/SplashScreen.js
+++ src/Views/SplashScreen.js
@@ -1,29 +1,83 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
 import ColorPalette from "../Utils/ColorPalette";
 import Fonts from "../Utils/Fonts";
 import { MyButton } from "../Utils/Buttons";
 import SeparatorComponent from "../Utils/Spacer";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 
 export default SplashScreen = (props) => {
-  function startHandler() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  function loginHandler() {
+  // setAuth info
+
     props.navigation.navigate("homepage-chats-index");
   }
 
+
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={stylesheet.container}>
       <StatusBar backgroundColor="auto" />
-      <SeparatorComponent space={"20%"} />
-      <Text style={styles.titleStyle}>Jay</Text>
+      <SeparatorComponent space={"5%"} />
+      <Text style={stylesheet.titleStyle}>Jay</Text>
+      <View
+        style={{
+          width: "70%",
+          marginHorizontal: 10,
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <View style={stylesheet.authFieldContainer}>
+          <Text style={stylesheet.labelsStyle}>Username</Text>
+          <TextInput
+            style={stylesheet.authInput}
+            selectionColor={ColorPalette.Blue}
+            color={ColorPalette.black}
+            autoCorrect={false}
+            placeholder={"Username"}
+            placeholderTextColor={ColorPalette.DarkGrey + 80}
+            maxLength={16}
+            numberOfLines={1}
+            value={username}
+            onChangeText={(text) => {
+              setUsername(text);
+            }}
+          />
+        </View>
+        <View style={stylesheet.authFieldContainer}>
+          <Text style={stylesheet.labelsStyle}>Password</Text>
+          <TextInput
+            style={stylesheet.authInput}
+            selectionColor={ColorPalette.Blue}
+            color={ColorPalette.black}
+            autoCorrect={false}
+            placeholder={"Password"}
+            placeholderTextColor={ColorPalette.DarkGrey + 80}
+            secureTextEntry={true}
+            textContentType={"password"}
+            value={password}
+            onChangeText={(text) => {
+              setPassword(text);
+            }}
+          />
+        </View>
+      </View>
+
       <MyButton
         bgColor={"transparent"}
         borderColor={ColorPalette.white}
         width={"90%"}
         height={50}
         borderRadius={10}
-        onPress={startHandler}
+        onPress={loginHandler}
       >
-        <Text style={styles.buttonText}>Start</Text>
+        <Text style={stylesheet.buttonText}>Login</Text>
       </MyButton>
 
       <Text
@@ -31,16 +85,16 @@ export default SplashScreen = (props) => {
           color: ColorPalette.white,
           fontFamily: Fonts.BodyText,
           fontSize: 11,
-          textAlign: "center"
+          textAlign: "center",
         }}
       >
-        A Prototype by Femke Kocken & M. Shahrom Ali.
+        A Speculative Design Prototype {"\n"} by Femke Kocken & M. Shahrom Ali.
       </Text>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
+const stylesheet = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: ColorPalette.black,
@@ -58,5 +112,29 @@ const styles = StyleSheet.create({
     color: ColorPalette.white,
     fontFamily: Fonts.Bold,
     fontSize: 20,
+  },
+
+  labelsStyle: {
+    color: ColorPalette.white,
+    fontFamily: Fonts.Subheading,
+    fontSize: 20,
+  },
+  authFieldContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  authInput: {
+    backgroundColor: ColorPalette.white,
+    fontSize: 14,
+    fontFamily: Fonts.Subheading,
+    borderColor: ColorPalette.DarkGrey + 50,
+    paddingHorizontal: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "left",
+    width: "60%",
+    height: 30,
   },
 });
