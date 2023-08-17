f5d08cc4acfdff39fdeb22d790444d792296fc5b
Shahrom <mshahromali@gmail.com>
2023-08-17 05:08:13 -0400

    basic chats index page

--- src/Views/ChatsHomepage.js
+++ src/Views/ChatsHomepage.js
@@ -1,12 +1,118 @@
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Pressable,
+  Alert,
+} from "react-native";
 import ColorPalette from "../Utils/ColorPalette";
 import Fonts from "../Utils/Fonts";
+import { Ionicons } from "@expo/vector-icons";
+
+const ChatItem = (props) => {
+  return (
+    <Pressable
+      style={{
+        margin: 10,
+        alignItems: "center",
+        flexDirection: "row",
+      }}
+      onPress={props.onPress}
+      onLongPress={props.longPress}
+      delayLongPress={300}
+    >
+      <View
+        style={{
+          borderRadius: 30,
+          width: 40,
+          height: 40,
+          backgroundColor: ColorPalette.Orange,
+          marginVertical: 10,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Ionicons name={"person"} color={ColorPalette.lightGrey} size={25} />
+      </View>
+      <Text
+        style={{
+          color: ColorPalette.white,
+          fontFamily: Fonts.Subheading,
+          fontSize: 18,
+          marginLeft: 10,
+        }}
+      >
+        Jay Mort
+      </Text>
+    </Pressable>
+  );
+};
 
 export default ChatsHomepage = (props) => {
+  function onPressSettingsHandler() {
+    props.navigation.navigate("settings-screen");
+  }
+
+  function newChatHandler() {
+    props.navigation.navigate("new-chats-screen");
+  }
+
+  function chatClickHandler() {
+    props.navigation.navigate("chat-screen");
+  }
+
+  function deleteChatHandler() {
+    //Delete chat
+    console.log("DEBUG:: Chat was deleted!");
+  }
+  function chatLongpressHandler() {
+    //Show delete option -> resets history
+    Alert.alert(
+      "Delete Chat?",
+      "This action cannot be undone.",
+      [
+        { text: "Cancel", style: "cancel" },
+        { text: "Delete", onPress: deleteChatHandler, style: "destructive" },
+      ]
+    );
+  }
+
   return (
     <SafeAreaView style={stylesheet.screenContainer}>
-      <Text style={stylesheet.pageTitle}>Chats</Text>
+      <View
+        style={{
+          borderBottomColor: ColorPalette.white,
+          padding: 10,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          borderBottomWidth: 1,
+        }}
+      >
+        <Text style={stylesheet.pageTitle}>Chats</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: 60,
+          }}
+        >
+          <Pressable onPress={newChatHandler}>
+            <Ionicons name={"add"} color={ColorPalette.white} size={20} />
+          </Pressable>
+          <Pressable onPress={onPressSettingsHandler}>
+            <Ionicons
+              name={"settings-outline"}
+              color={ColorPalette.white}
+              size={20}
+            />
+          </Pressable>
+        </View>
+      </View>
+
+      <ChatItem onPress={chatClickHandler} longPress={chatLongpressHandler} />
     </SafeAreaView>
   );
 };
@@ -20,7 +126,7 @@ const stylesheet = StyleSheet.create({
   },
   pageTitle: {
     fontFamily: Fonts.Title,
-    fontSize: 20,
+    fontSize: 25,
     color: ColorPalette.white,
   },
 });
