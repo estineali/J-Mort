fee2633097f6a3a5369373d876ebe146382a4357
Shahrom <mshahromali@gmail.com>
2023-08-17 13:58:17 -0400

    conditional rendering of initial route

--- src/appNavigation.js
+++ src/appNavigation.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SplashScreen from "./Views/SplashScreen";
 import ChatsHomepage from "./Views/ChatsHomepage";
 import ChatScreen from "./Views/ChatScreen";
 import SettingsScreen from "./Views/SettingsScreen";
 import NewChatsScreen from "./Views/NewChatsScreen";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { keys } from "./Utils/asyncstorageKeys";
 
 const NavStack = createNativeStackNavigator();
 
 export default Screens = () => {
+  const [initRoute, setInitRoute] = useState("splash-screen");
+
+  async function loadAuth() {
+    const userName = await AsyncStorage.getItem(keys.userName);
+    
+    console.log("DEBUG:username is " + userName);
+    if (userName) {
+      setInitRoute("homepage-chats-index");
+    }
+  }
+
+  useEffect(() => {
+    loadAuth();
+  }, []);
+
   return (
     <NavStack.Navigator
-      initialRouteName="splash-screen"
+      initialRouteName={initRoute}
       screenOptions={{ headerShown: false }}
     >
       <NavStack.Screen name="splash-screen" component={SplashScreen} />
